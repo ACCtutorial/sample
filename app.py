2315e70aaedac23afd9dce68eb612c1500eee720
balajid <balajid@dal.ca>
2020-02-09 21:39:59 -0400

    files updated

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template
 import os
 import random
-import pandas as pd
-import psycopg2
+# import pandas as pd
+# import psycopg2
 
 app = Flask(__name__)
 
-connection=psycopg2.connect(host='flask.cbhu7ev8prgh.us-east-1.rds.amazonaws.com',
-                            port=5432,
-                            user='postgres',
-                            password='postgres',
-                            database='flask')
+# connection=psycopg2.connect(host='flask.cbhu7ev8prgh.us-east-1.rds.amazonaws.com',
+#                             port=5432,
+#                             user='postgres',
+#                             password='postgres',
+#                             database='flask')
 
-cursor=connection.cursor()
+# cursor=connection.cursor()
 
-sql="""select * from passengers"""
+# sql="""select * from passengers"""
 
-demo=pd.read_sql(sql,con=connection)
+# demo=pd.read_sql(sql,con=connection)
 
 # list of cat images
 image = "https://dalu-my.sharepoint.com/:i:/r/personal/bl977277_dal_ca/Documents/docker.png?csf=1&e=CeXcwR"
@@ -25,9 +25,9 @@ image = "https://dalu-my.sharepoint.com/:i:/r/personal/bl977277_dal_ca/Documents
 @app.route("/")
 def index():
     url = image
-    name = demo.name[0]
-    age = demo.age[0]
-    return render_template("index.html", url=url,name=name,age=age)
+    # name = demo.name[0]
+    # age = demo.age[0]
+    return render_template("index.html", url=url)
 
 
 if __name__ == "__main__":
